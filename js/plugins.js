5f04c58537e8a43fd6d95b4b5b3748eb34ccd882
bsptetu <55188675+bsptetu@users.noreply.github.com>
2021-01-19 13:46:07 +0900

    20210119

--- js/plugins.js
+++ js/plugins.js
@@ -3,7 +3,7 @@
 var $plugins =
 [
 {"name":"Community_Basic","status":true,"description":"基本的なパラメーターを設定するプラグインです。","parameters":{"cacheLimit":"20","screenWidth":"1350","screenHeight":"725","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"canvas","alwaysDash":"off","textSpeed":"1","autoSaveFileId":"0","errorMessage":"エラーが発生しました。ゲームの作者にご連絡ください。","showErrorDetail":"true","enableProgressBar":"true","maxRenderingFps":"0"}},
-{"name":"TitleNewGameOnly","status":true,"description":"ニューゲームオンリープラグイン","parameters":{"startString":"Login start","font":"","fileExistAction":"0","soundEffect":"","adjustY":"0"}},
+{"name":"TitleNewGameOnly","status":true,"description":"ニューゲームオンリープラグイン","parameters":{"startString":"","font":"","fileExistAction":"0","soundEffect":"","adjustY":"0"}},
 {"name":"OnlineAvatar","status":true,"description":"Firebaseを使ってプレイヤーをオンライン同期します。","parameters":{"apiKey":"AIzaSyDIX1x4RY9IidqLg6UQBms6FPxtagV9DW4","authDomain":"bisp-44fc5.firebaseapp.com","databaseURL":"https://bisp-44fc5-default-rtdb.firebaseio.com","avatarEvent":"0","syncSwitchStart":"0","syncSwitchEnd":"0","syncVariableStart":"450","syncVariableEnd":"5000"}},
 {"name":"PictureCallCommon","status":true,"description":"ピクチャのボタン化プラグイン","parameters":{"透明色を考慮":"true","ピクチャ番号の変数番号":"0","ポインタX座標の変数番号":"0","ポインタY座標の変数番号":"0","タッチ操作抑制":"false","戦闘中常にコモン実行":"false","無効スイッチ":"0"}},
 {"name":"DTextPicture","status":true,"description":"動的文字列ピクチャ生成プラグイン","parameters":{}},
